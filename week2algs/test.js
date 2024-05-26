c49fb62f29b33105dd973759a847c1afe6d900a1
Atwan-Sha <atwanshaaban@gmail.com>
2024-05-26 17:13:46 +0200

    test

--- week2algs/test.js
+++ week2algs/test.js
@@ -2,8 +2,13 @@ console.log('TEST file...');
 
 arr = [5,6,7,8,9];
 for(let i = 0; i < arr.length; i++){
-    console.log(arr[i]);
+    // console.log(arr[i]);
     i++;
 }
+
+// arr.forEach(e => console.log(e));
+console.log(arr.filter(e => e > 6));
+
+
 
 
